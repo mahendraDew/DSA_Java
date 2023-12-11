1c40dad6e8200d4668bf9c34fe29a8ae223fc0dc
“MahendraDew” <mahendradewangan195@gmail.com>
2023-12-11 22:03:00 +0530

    neetcode -array and hashmap -kfrequentele

---
+++ Neetcode-150/Arrays & Hashing/KFrequentEle.java
@@ -0,0 +1,26 @@
+// 347. Top K Frequent Elements:https://leetcode.com/problems/top-k-frequent-elements/
+
+class KFrequentEle{
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>(map.values());
+        Collections.sort(list, Collections.reverseOrder());
+
+        ArrayList<Integer> list2 = new ArrayList<>(list.subList(0, k));
+
+        int[] ans = new int[list2.size()];
+        int j = 0;
+        for (var entry : map.entrySet()) {
+            if (list2.contains(entry.getValue())) {
+                ans[j++] = entry.getKey();
+            }
+        }
+
+        return ans;
+    }
+    
+}
