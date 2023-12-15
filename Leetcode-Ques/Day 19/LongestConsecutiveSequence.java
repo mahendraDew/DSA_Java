bec0ff0199eeb8503bed5b4bf2005144d7e848d2
“MahendraDew” <mahendradewangan195@gmail.com>
2023-12-15 20:44:39 +0530

    day19/100 -neetcode array que

---
+++ Leetcode-Ques/Day 19/LongestConsecutiveSequence.java
@@ -0,0 +1,28 @@
+//128. Longest Consecutive Sequence: https://leetcode.com/problems/longest-consecutive-sequence/description/
+
+class LongestConsecutiveSequence{
+    public int longestConsecutive(int[] nums) {
+        if(nums.length <= 1){
+            return nums.length;
+        }
+        int count = 1;
+        int result = 1;
+        Arrays.sort(nums);
+
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == nums[i-1] + 1){
+                count++;
+            }
+            else if(nums[i] == nums[i-1]){
+                continue;
+            }
+            else{
+                result = Math.max(result, count);
+                count = 1;
+            }
+        
+        }
+        result = Math.max(result, count);
+        return result;
+    }
+}
