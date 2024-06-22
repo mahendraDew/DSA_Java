6c7c000d13261643f473932c1fad83dbde25c8f2
“MahendraDew” <mahendradewangan195@gmail.com>
2024-06-22 12:00:11 +0530

    leetcode day 27 -22june

---
+++ Leetcode_Ques/Array/RobHouse.java
@@ -0,0 +1,20 @@
+package Leetcode_Ques.Array;
+
+public class RobHouse {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,1,1,3};
+        System.out.println(rob(arr));
+    }
+    private static int rob(int[] nums){
+        int prevMax = nums[0];
+        int max = Math.max(prevMax, nums[1]);
+
+        for(int i = 2; i<nums.length; i++){
+            int curr = Math.max(max, prevMax + nums[i]);
+            prevMax = max;
+            max = curr; 
+        }
+
+        return max;
+    }
+}
