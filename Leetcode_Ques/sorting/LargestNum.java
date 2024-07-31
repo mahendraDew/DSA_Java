56d3aac9efe90b02d6ec8371b92bb101df1f3805
“MahendraDew” <mahendradewangan195@gmail.com>
2024-07-31 11:55:40 +0530

    added lc ques largestNum and KlargestEleArr

---
+++ Leetcode_Ques/sorting/LargestNum.java
@@ -0,0 +1,24 @@
+package Leetcode_Ques.sorting;
+
+import java.util.Arrays;
+
+public class LargestNum {
+     public static String largestNumber(int[] nums) {
+        String[] str = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            str[i] = String.valueOf(nums[i]);
+        }
+        StringBuilder sb = new StringBuilder("");
+        Arrays.sort(str, (a,b)->(b+a).compareTo(a+b));
+        for(String s: str){
+            sb.append(s);
+        }
+        String res = sb.toString();
+        return res.startsWith("0")? "0" : res;
+    }
+    public static void main(String[] args) {
+        int[] nums = {};
+        String str = largestNumber(nums);
+        System.out.println(str);
+    }
+}
