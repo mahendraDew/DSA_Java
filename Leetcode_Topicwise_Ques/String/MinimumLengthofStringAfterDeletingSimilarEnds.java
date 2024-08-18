826469fddb2da378e78754b328bfc721691e53b8
mahendra dewangan <mahendradewangan195@gmail.com>
2024-08-18 12:23:34 +0530

    updated the dir, added LC Topicwise Ques

---
+++ Leetcode_Topicwise_Ques/String/MinimumLengthofStringAfterDeletingSimilarEnds.java
@@ -0,0 +1,22 @@
+public class MinimumLengthofStringAfterDeletingSimilarEnds {
+    public static void main(String[] args) {
+        String  s = "cabaabasac";
+        System.out.println(minimumLength(s));
+    }
+
+    public static int minimumLength(String s) {
+       int start = 0;
+       int end = s.length()-1;
+       while(start<end && s.charAt(start) == s.charAt(end)){
+          char c = s.charAt(start);
+          while(start<=end && s.charAt(start) == c){
+            start++;
+          }
+          while(start<end && s.charAt(end) == c){
+            end--;
+          }
+       }
+
+       return end - start +1;
+    }
+}
