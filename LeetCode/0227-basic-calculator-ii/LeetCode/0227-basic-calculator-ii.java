b7bd1c96ac0cf9943140b200c7d2bd8b476ccc30
Mahendra Dewangan <98692513+mahendraDew@users.noreply.github.com>
2024-08-18 11:44:40 +0530

    Time: 11 ms (94.86%), Space: 44.5 MB (95.13%) - LeetHub

--- LeetCode/0227-basic-calculator-ii/LeetCode/0227-basic-calculator-ii.java
+++ LeetCode/0227-basic-calculator-ii/LeetCode/0227-basic-calculator-ii.java
@@ -1,12 +1,48 @@
 class Solution {
     public int calculate(String s) {
+        //using stack
+//         if(s == null || s.length() == 0){
+//             return 0;
+//         }
+        
+//         int curr = 0;
+//         char op = '+';
+//         Stack<Integer> st = new Stack();
+//         char[] ch = s.toCharArray();
+//         for(int i = 0; i<ch.length; i++){
+//             if(Character.isDigit(ch[i])){
+//                 curr = curr*10 + ch[i] - '0';
+//             }
+//             if(!Character.isDigit(ch[i]) && ch[i] != ' ' || i == ch.length-1){
+//                 if(op == '+'){
+//                     st.push(curr);
+//                 }else if(op == '-'){
+//                     st.push(-curr);
+//                 }else if(op == '*'){
+//                     st.push(st.pop() * curr);
+//                 }else if(op == '/'){
+//                     st.push(st.pop() / curr);
+//                 }
+                
+//                 op = ch[i];
+//                 curr = 0;
+//             }
+//         }
+//         int ans = 0;
+//         while(!st.isEmpty()){
+//             ans += st.pop();
+//         }
+//         return ans;
+        
+        //without stack
         if(s == null || s.length() == 0){
             return 0;
         }
         
         int curr = 0;
         char op = '+';
-        Stack<Integer> st = new Stack();
+        int last = 0;
+        int sum = 0;
         char[] ch = s.toCharArray();
         for(int i = 0; i<ch.length; i++){
             if(Character.isDigit(ch[i])){
@@ -14,23 +50,23 @@ class Solution {
             }
             if(!Character.isDigit(ch[i]) && ch[i] != ' ' || i == ch.length-1){
                 if(op == '+'){
-                    st.push(curr);
+                    sum += last;
+                   last = curr;
                 }else if(op == '-'){
-                    st.push(-curr);
+                    sum += last;
+                    last = -curr;
                 }else if(op == '*'){
-                    st.push(st.pop() * curr);
+                    last = last*curr;
                 }else if(op == '/'){
-                    st.push(st.pop() / curr);
+                    last = last/curr;
                 }
                 
                 op = ch[i];
                 curr = 0;
             }
         }
-        int ans = 0;
-        while(!st.isEmpty()){
-            ans += st.pop();
-        }
-        return ans;
+        sum += last;
+        return sum;
+       
     }
 }
